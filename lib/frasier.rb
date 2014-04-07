d8118b809508be2c08ca8a30a1e01ac9fd09c41a
pjaspers <junkiesxl@gmail.com>
2014-04-07 22:51:43 +0200

    Adding inspirational quote to replace lack of code.

--- lib/frasier.rb
+++ lib/frasier.rb
@@ -6,5 +6,15 @@ require "frasier/generator"
 require "frasier/cli"
 
 module Frasier
-  # Your code goes here...
+
+  # At Cornell University, they have an incredible piece of scientific
+  # equipment known as the "tunneling electron microscope."
+  #
+  # Now, this microscope is so powerful that by firing electrons, you
+  # can actually see images of the atom, the infinite decimally minute
+  # building block of our universe.
+  #
+  # Roger, if I were using that microscope right now, I still wouldn't
+  # be able to locate my interest in your problem.
+
 end
