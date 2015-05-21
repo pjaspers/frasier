b5d2aecdafc664a9903a93358dc3d83275191002
pjaspers <piet@pjaspers.com>
2015-05-22 00:46:07 +0200

    Step away from OpenStruct
    Remember: Every time you use OpenStruct.new, Ruby blows its entire
    method cache.

--- lib/frasier/cli.rb
+++ lib/frasier/cli.rb
@@ -1,18 +1,17 @@
-require "ostruct"
 require "optparse"
 
 module Frasier
 
   class CLI
     def self.parse(args)
-      options = OpenStruct.new
-      options.number_of_words = 5
+      options = {}
+      options[:number_of_words] = 5
 
       opts = OptionParser.new do |opts|
         opts.banner = "Usage: frasier [options]"
 
         opts.on("-n", "--number [NUMBER]", Integer, "Generate passphrase with <n> words") do |n|
-          options.number_of_words = n.to_i
+          options[:number_of_words] = n.to_i
         end
 
         opts.on("-l", "--list-books", "List available books") do |list|
@@ -33,12 +32,16 @@ module Frasier
           options.book = book
         end
 
+        opts.on("-c", "--max-chars [NUMBER]", Integer, "Maximum number of chars") do |n|
+          options[:max_chars] = n.to_i
+        end
+
         opts.on("-i", "--info", "Show entropy info") do |info|
-          options.info = info
+          options[:info] = info
         end
 
         opts.on("-S", "--no-whitespace", "Replace whitespace with random characters") do |bool|
-          options.no_whitespace = !bool
+          options[:no_whitespace] = !bool
         end
 
         opts.on_tail("-h", "--help", "Show this message") do
@@ -68,28 +71,33 @@ BLURB
 
     def initialize(options)
       library = Library.new
-      @book = library.book_with_name(options.book) if options.book
-      @book = library.random_book unless @book
-      number_of_words = options.number_of_words
-
-      @generator = Generator.new(@book.dice_word_list, number_of_words)
-      print_passphrase(options.info, !!options.no_whitespace)
+      @book = library.book_with_name(options.fetch(:book, library.random_book.title))
+      @generator = Generator.new(@book.dice_word_list, options)
+      print_passphrase(options[:info])
     end
 
-    def print_passphrase(info = true, no_whitespace = false)
-      if no_whitespace
-        phrase = @generator.passphrase_without_whitespace
-      else
-        phrase = @generator.passphrase
-      end
+    def print_passphrase(info = true)
+      phrase = @generator.passphrase
+
       # Try to copy it
       copy(phrase) if copy_command
       if info
         number_of_guesses = 100000
-        duration_in_years = (@generator.duration_to_guess(number_of_guesses)/60.0/60.0/24.0/360.0).round(2)
+        duration_in_days = (@generator.duration_to_guess(number_of_guesses)/60.0/60.0/24.0)
+        duration_in_months = duration_in_days/30.0
+        duration_in_years = duration_in_months/12.0
+
         puts "From: #{@book.title}"
         puts "Bits of entropy: #{@generator.bits_of_entropy}"
-        puts "At 100 000 guesses/s, it would take %s years to guess" % duration_in_years
+        if duration_in_years > 1
+          puts "At 100 000 guesses/s, it would take %s years to guess" % duration_in_years.round(2)
+        elsif duration_in_months > 1
+          puts "At 100 000 guesses/s, it would take %s months to guess" % duration_in_months.round(2)
+        elsif duration_in_days > 1
+          puts "At 100 000 guesses/s, it would take %s months to guess" % duration_in_days.round(2)
+        else
+          puts "At 100 000 guesses/s, it would take literally no time to guess"
+        end
         puts ""
       end
       puts red(phrase)
